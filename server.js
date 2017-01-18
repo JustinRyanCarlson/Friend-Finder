a3b87c1d7d8a3560915168b4b54e442a1fcbfc10
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-17 18:55:30 -0600

    adds validate class to select elements in survey.html, creates server.js and does intial setup for port 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.listen(PORT, function() {
+    console.log('Listening on port: ' + PORT);
+});
