a3a9aa77e53c49511bf4caca86084ff66ac85f91
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-19 17:34:05 -0600

    adds body-parser to apiRoutes.js, adds console.log to the post of apiRoutes.js to make sure im getting the correct data from the survey form, removes body parser from server.js as its not needed, adds construction of the newFriendData object and post this data to survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,21 @@
 var friends = require('../data/friends.js');
+var bodyParser = require('body-parser');
+
+
 
 module.exports = function(app) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.text());
+    app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
     app.get('/api/friends', function(req, res) {
         res.end(JSON.stringify(friends));
     });
 
     app.post('/api/friends', function(req, res) {
-
+        var newFriend = req.body;
+        console.log(newFriend);
     });
 
 };
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 var PORT = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.listen(PORT, function() {
     console.log('Listening on port: ' + PORT);
