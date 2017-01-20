b70c6a61a276f7f05768b6a64c143e04198a0aec
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-19 20:18:51 -0600

    adds working post function to match friends in apiRoutes.js, adds modal for if the user hasnt populated all fields in survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,30 +9,33 @@ module.exports = function(app) {
     app.use(bodyParser.text());
     app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+
     app.get('/api/friends', function(req, res) {
         res.end(JSON.stringify(friends));
     });
 
+
     app.post('/api/friends', function(req, res) {
         var newFriend = req.body;
         var scoreRelations = [];
-        var difference = 0;
 
         for (var i = 0; i < friends.length; i++) {
             var currentIndexScoreRelations = [];
+            var difference = 0;
+
             for (var j = 0; j < friends[i].scores.length; j++) {
                 currentIndexScoreRelations.push(Math.abs(newFriend.scores[j] - friends[i].scores[j]));
             }
+
             for (var k = 0; k < currentIndexScoreRelations.length; k++) {
                 difference += currentIndexScoreRelations[k];
             }
+
             scoreRelations.push(difference);
         }
 
         var minArrNumber = Math.min(...scoreRelations);
         var indexOfMin = scoreRelations.indexOf(minArrNumber);
-        console.log(friends[indexOfMin]);
         res.json(friends[indexOfMin]);
     });
-
 };
