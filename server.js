14e02ff7fd4fe578261a2278f55c1b7a950a2273
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-21 09:32:49 -0600

    edits comment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 var PORT = process.env.PORT || 3000;
 
-// Starts listening on localhost:3000
+// Starts listening on localhost:3000 or the enviromental port.
 app.listen(PORT, function() {
     console.log('Listening on port: ' + PORT);
 });
