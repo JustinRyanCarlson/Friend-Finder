cd10a2466d52325e42cdeb2fbb130b3120720951
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-20 19:04:02 -0600

    comments server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+// Requires the routes for this application.
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 var PORT = 3000;
 
+// Starts listening on localhost:3000
 app.listen(PORT, function() {
     console.log('Listening on port: ' + PORT);
 });
