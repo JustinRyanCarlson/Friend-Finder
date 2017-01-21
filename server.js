b9d87052901a57332d98e644aa0ccffc6e10d676
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-21 09:29:14 -0600

    adds enviromental port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 // Requires the routes for this application.
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Starts listening on localhost:3000
 app.listen(PORT, function() {
